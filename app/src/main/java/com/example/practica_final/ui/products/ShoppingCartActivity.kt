7ce908abb330bdce98083009b8ae6a9d3037671b
Carlos Abdel Pacehco Martínez <Pacheco.mc21@gmail.com>
2023-05-24 12:07:31 -0600

    Se agrego funcionalidad a finalizar compre se corriguio la carga de productos en shoppingcart

--- app/src/main/java/com/example/practica_final/ui/products/ShoppingCartActivity.kt
+++ app/src/main/java/com/example/practica_final/ui/products/ShoppingCartActivity.kt
@@ -1,12 +1,17 @@
 package com.example.practica_final.ui.products
 
 import android.os.Bundle
+import android.widget.Button
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.practica_final.R
+import com.example.practica_final.databinding.FragmentShoppingcartBinding
 import com.example.practica_final.domain.Product
+import com.example.practica_final.domain.ProductAdapter
+import com.example.practica_final.domain.ProductProvider.Companion.productList
 import com.example.practica_final.ui.adapter.ProductAdapterNew
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.DataSnapshot
@@ -23,9 +28,33 @@ class ShoppingCartActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.fragment_shoppingcart)
 
+        val confirmBuyButton = findViewById<Button>(R.id.btn_end_shopping)
+        confirmBuyButton.setOnClickListener {
+            mostrarDialogoConfirmacion()
+        }
         recyclerView = findViewById(R.id.lst_shopingcart)
         recyclerView.layoutManager = LinearLayoutManager(this)
+
         fetchCartProducts()
+    }
+    private fun mostrarDialogoConfirmacion() {
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle("Confirmar compra")
+        builder.setMessage("¿Deseas realizar la compra?")
+        builder.setPositiveButton("Sí") { dialog, which ->
+
+            val firebaseDatabase = FirebaseDatabase.getInstance()
+            val user = FirebaseAuth.getInstance().currentUser
+            val userId = user?.uid
+            val cartRef = firebaseDatabase.getReference("carritos").child(userId.toString())
+            cartRef.removeValue()
+
+            Toast.makeText(this, "Compra realizada", Toast.LENGTH_SHORT).show()
+        }
+        builder.setNegativeButton("No") { dialog, which ->
+            Toast.makeText(this, "Compra cancelada", Toast.LENGTH_SHORT).show()
+        }
+        builder.show()
     }
 
     private fun fetchCartProducts() {
